c804bfbb3c5a14bcbacbf9ab3160855b02f8b86b
Mikael Vinhas <mikael_vinhas@hotmail.com>
2024-03-23 12:04:12 -0300

    adjust to make tests independent of mass

--- cypress/e2e/backend.cy.js
+++ cypress/e2e/backend.cy.js
@@ -1,11 +1,7 @@
 import { faker } from '@faker-js/faker'
 
-const USER_ID = Cypress.env('USER_ID')
-const USER_EMAIL_LOGIN = Cypress.env('USER_EMAIL_LOGIN')
 const USER_PASSWORD = Cypress.env('USER_PASSWORD')
 const API_URL = Cypress.env('API_BASE_URL')
-
-const USER_EDIT = Cypress.env('USER_EDIT')
 const NAME_EDIT = faker.internet.userName()
 const EMAIL_EDIT = faker.internet.email()
 const PASSWORD_EDIT = faker.internet.password()
@@ -16,38 +12,95 @@ let userId;
 describe('api testing', () => {
 
   it('retrieves user information', () => {
+    const GET_EMAIL_USER = faker.internet.email()
 
-    cy.getUserInfo(USER_ID).should(({ status, body }) => {
-      expect(status).to.eq(200)
-      expect(body.quantidade).to.eq(1)
-      expect(body.usuarios[0].administrador).to.eq('true')
-      expect(body.usuarios[0].email).to.eq(Cypress.env('USER_EMAIL'))
-      expect(body.usuarios[0].nome).to.eq(Cypress.env('USER_NAME'))
-      expect(body.usuarios[0].password).to.exist
-      expect(body.usuarios[0]._id).to.exist
+    // create user
+    cy.request({
+      method: 'POST',
+      url: `${API_URL}usuarios`,
+      body: {
+        "nome": "Mike Tyson",
+        "email": GET_EMAIL_USER,
+        "password": "teste",
+        "administrador": "true"
+      }
+    }).then((response) => {
+      expect(response).property('status').to.equal(201)
+      expect(response.body).property('_id').to.not.be.oneOf([null, ""])
+      const body = (response.body)
+      userId = body['_id']
+
+      // get user
+      cy.getUserInfo(userId).should(({ status, body }) => {
+        expect(status).to.eq(200)
+        expect(body.quantidade).to.eq(1)
+        expect(body.usuarios[0].administrador).to.eq('true')
+        expect(body.usuarios[0].email).to.eq(GET_EMAIL_USER)
+        expect(body.usuarios[0].nome).to.eq('Mike Tyson')
+        expect(body.usuarios[0].password).to.exist
+        expect(body.usuarios[0]._id).to.exist
+      })
     })
   })
 
   it('user login', () => {
+    const POST_EMAIL_USER = faker.internet.email()
 
-    cy.userLogin(USER_EMAIL_LOGIN, USER_PASSWORD).should(({ status, body }) => {
-      const { message, authorization } = body
+    // create user
+    cy.request({
+      method: 'POST',
+      url: `${API_URL}usuarios`,
+      body: {
+        "nome": "James Tyson",
+        "email": POST_EMAIL_USER,
+        "password": "teste",
+        "administrador": "true"
+      }
+    }).then((response) => {
+      expect(response).property('status').to.equal(201)
+      expect(response.body).property('_id').to.not.be.oneOf([null, ""])
+      const body = (response.body)
+      userId = body['_id']
 
-      expect(status).to.eq(200)
-      expect(message).to.eq('Login realizado com sucesso')
-      expect(authorization).to.exist
+      // login
+      cy.userLogin(POST_EMAIL_USER, USER_PASSWORD).should(({ status, body }) => {
+        const { message, authorization } = body
+        expect(status).to.eq(200)
+        expect(message).to.eq('Login realizado com sucesso')
+        expect(authorization).to.exist
+      })
     })
   })
 
   it('edit user', () => {
+    const PUT_EMAIL_USER = faker.internet.email()
 
-    cy.editUser(USER_EDIT, NAME_EDIT, EMAIL_EDIT, PASSWORD_EDIT, ADM_EDIT).should(({ status, body }) => {
-      const { message } = body
+    // create user
+    cy.request({
+      method: 'POST',
+      url: `${API_URL}usuarios`,
+      body: {
+        "nome": "Carl Tyson",
+        "email": PUT_EMAIL_USER,
+        "password": "teste",
+        "administrador": "true"
+      }
+    }).then((response) => {
+      expect(response).property('status').to.equal(201)
+      expect(response.body).property('_id').to.not.be.oneOf([null, ""])
+      const body = (response.body)
+      userId = body['_id']
 
-      expect(status).to.eq(200)
-      expect(message).to.eq('Registro alterado com sucesso')
+      // edit user
+      cy.editUser(userId, NAME_EDIT, EMAIL_EDIT, PASSWORD_EDIT, ADM_EDIT).should(({ status, body }) => {
+        const { message } = body
+
+        expect(status).to.eq(200)
+        expect(message).to.eq('Registro alterado com sucesso')
+      })
     })
   })
+
 
   it('delete user', () => {
 
